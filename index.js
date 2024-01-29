51e9e6347624580e997aae9dcd332706efed92e7
Wangyi <61499676+ravenjuce@users.noreply.github.com>
2024-01-29 12:46:18 +1100

    Initialised with basic function

---
+++ index.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+
+function generatePassword() {
+    document.getElementById("el-1").textContent=generateOnePassword()
+    document.getElementById("el-2").textContent=generateOnePassword()
+}
+
+
+function generateOnePassword() {
+    let pwd = ""
+    for(let i=0; i<15; i++){
+        pwd += characters[Math.floor( Math.random()*(characters.length-1) )+1]
+    }
+    return pwd
+}
+
+
